cdc2b70bc3f7d546fceac359fd2e69fa2c16d3b0
SimonStorlSchulke <simonstoschu@googlemail.com>
2025-03-02 01:56:23 +0100

    fix uploads cleaner script

--- public/cleanup-uploads.py
+++ public/cleanup-uploads.py
@@ -2,7 +2,7 @@ import os
 
 folder_path = "./uploads"
 
-allowed_prefixes = ("thumbnail_", "_smallsmall_", "medium_", "large_", "xlarge_")
+allowed_prefixes = ("thumbnail_", "small_", "medium_", "large_", "xlarge_")
 
 for filename in os.listdir(folder_path):
     file_path = os.path.join(folder_path, filename)
