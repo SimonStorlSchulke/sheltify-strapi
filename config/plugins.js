af96ea64c149dc57e260324015f202aad3b8cd6e
SimonDerZauberzwerg <simonstoschu@gmail.com>
2025-08-06 21:02:48 +0200

    change mailprovider

--- config/plugins.js
+++ config/plugins.js
@@ -1,35 +1,40 @@
-module.exports = ({env}) => ({
-    ezforms: {
-        config:{
-          captchaProvider: {
-            name: 'none',
-          },
-          notificationProviders: []
-        },
+module.exports = ({ env }) => ({
+  ezforms: {
+    config: {
+      captchaProvider: {
+        name: 'none',
+      },
+      notificationProviders: []
     },
-    upload: {
-      config: {
-        breakpoints: {
-          xlarge: 1920,
-          large: 1000,
-          medium: 750,
-          small: 500,
-        },
-      }
-    },
-    email: {
-      config: {
-        provider: 'sendgrid',
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-          defaultFrom: env('SENDGRID_DEFAULT_FROM'),
-          defaultReplyTo: env('SENDGRID_DEFAULT_TO'),
+  },
+  upload: {
+    config: {
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+      },
+    }
+  },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+          user: env('GMAIL_USERNAME'),
+          pass: env('GMAIL_PASSWORD'),
         },
       },
+      settings: {
+        defaultFrom: env('GMAIL_USERNAME'),
+        defaultReplyTo: env('GMAIL_USERNAME'),
+      },
     },
-    'users-permissions': {
+  },
+  'users-permissions': {
     config: {
       ratelimit: {
         // 1 mail every 5sec allowed
